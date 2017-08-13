669627b14929f1b58a49b9dd349c90391bcf3907
Ashish Karki <u2aashish@gmail.com>
2017-08-13 15:02:46 -0400

    Initial commit with all specified functionality

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2083915.json',
+    dataType: 'jsonp',
+    success: function(response){
+      var badges = $('#badges');
+      $.each(response.courses.completed, function(index, value){
+        var badge = '<div class="course">';
+        badge += '<h3>' + value.title + '</h3>';
+        badge += '<img src=' + value.badge + '>';
+        badge += '<a href="' + value.url + '" class="btn btn-primary" target="_blank">See Course</a>';  
+        badge += '</div>';
+        console.log(JSON.stringify(value));
+        badges.append(badge);
+      });
+    }
+  });
 });
